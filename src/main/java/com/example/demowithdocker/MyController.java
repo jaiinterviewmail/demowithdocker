c8ac6da376f17422a87099e39983f19ea5fbdd7e
Kaluthoti <jaya-kumar.kaluthoti@capgemini.com>
2026-05-18 14:57:01 +0530

    Initial commit

---
+++ src/main/java/com/example/demowithdocker/MyController.java
@@ -0,0 +1,11 @@
+package com.example.demowithdocker;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+ public String getString(){
+     return "hai";
+ }
+
+}
